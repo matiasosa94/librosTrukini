555e7b1c9b596e0c8e93facfcb77a662196be843
Matias Sosa <matiasosa94@gmail.com>
2022-10-31 01:13:30 -0300

    1

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const cookies = require('cookie-parser')
+const app = express();
+
+app.set("view engine", "ejs");
+
+const publicPath=path.resolve(__dirname,'./public');
+app.use(express.static(publicPath));
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookies());
+app.use(session({
+    secret:"Esto es un secreto",
+    resave:false,
+    saveUninitialized:false,
+}));
+
+
+
+
+
+app.listen(process.env.PORT || 3015, ()=>{
+    console.log("Servidor corriendo");
+});
+
